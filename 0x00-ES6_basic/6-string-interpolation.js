a793a8d6e3961dc2ecd4739a2170ca4e5c9d6e74
kabingusam <kabingusammy@gmail.com>
2022-12-20 13:58:41 +0300

    update on task

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,4 +1,4 @@
-getSanFranciscoDescription = () => {
+export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
     income: '$119,868',
@@ -6,7 +6,5 @@ getSanFranciscoDescription = () => {
     capita: '$178,479',
   };
 
-  return `As of   ${year}  , it was the seventh-highest income county in the United States
-         , with a per capita personal income of   ${income}  . As of 2015, San Francisco
-          proper had a GDP of   ${gdp}  , and a GDP per capita of   ${capita}  .`;
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
