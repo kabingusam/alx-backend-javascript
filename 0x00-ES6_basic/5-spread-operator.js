848073d224cf20d888ce3074580f3682996d41cf
kabingusam <kabingusammy@gmail.com>
2022-12-20 12:09:14 +0300

    update on typo

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-    return  [...array1, ...array2, 'H', 'e', 'l', 'l', 'o'];
+    return([...array1, ...array2, 'H', 'e', 'l', 'l', 'o']);
 }
