9b678807bd8ec434f29fd2914978602ebd7f318a
kabingusam <kabingusammy@gmail.com>
2022-12-20 12:01:00 +0300

    update on complete tasks

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log("hello");
+// this returns reference to an object
+// const person = {
+//     name: "sam",
+//     walk() {
+//         console.log(this)
+//     }
+// };
+// person.walk();
+
+
+// --> Arrow functions
+
+// const square = function (number) {
+//     return number * number;
+// } 
+
+// const square = number => number * number;
+// // console.log( square(5));
+
+// const jobs = [
+//     {id: 1, isActive: true},
+//     {id: 2, isActive: true},
+//     {id: 3, isActive: false}
+// ]
+// const activeJobs = jobs.filter(function(jobs) { return jobs.isActive; });
+// const activeJobs = jobs.filter(jobs => jobs.isActive);
+// console.log(activeJobs);
+
+
+// -> Arrow functions with this keyword
+
+// const person = {
+//     talk() {
+//         var self = this;
+//         setTimeout (function() {
+//             console.log("self", self);
+//         }, 1000);
+//     }
+// };
+
+// person.talk();
+
+// const person = {
+//     talk() {
+//         var self = this;
+//         setTimeout (() => {
+//             console.log("this", this);
+//         }, 1000);
+//     }
+// };
+
+// person.talk();
+
+concatArrays = (array1, array2, string) =>  [...array1, ...array2, 'H', 'e', 'l', 'l', 'o'];
+
+console.log(concatArrays(['a', 'b'], ['c', 'd'], 'hello'));
