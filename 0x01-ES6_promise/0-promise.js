c68e66ed8376c1499c6e043c281ab41e91e667b3
kabingusam <kabingusammy@gmail.com>
2022-12-21 12:02:04 +0300

    update on task

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,11 @@
+export function getResponseFromAPI() {
+    const response = new Promise((resolve) => {
+        resolve('true')
+    })
+}
 getResponseFromAPI()
     .then(() => {
         resolve(true)
-}).catch(() => {
+}).catch((error) => {
     reject(false)
 })
