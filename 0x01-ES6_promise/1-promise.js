29354392e1057b690afb9d07be94784104fa8b7a
kabingusam <kabingusammy@gmail.com>
2022-12-21 11:47:12 +0300

    update on task

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,12 @@
-function getFullResponseFromAPI(success) {
-    success = new Promise((resolve, reject) => {
-        if(success = true) {
+function getResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+        if(success){
             resolve({
                 status: 200,
-                body: 'Success'
-            })
-        } else {
-            reject({
-                message: 'The fake API is not working currently',
-            })
+                body: 'Success',
+            });
+        }else {
+            reject(new Error('The fake API is not working currently'))
         }
-    }) 
+    });
 }
