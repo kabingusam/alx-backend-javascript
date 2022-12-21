9c7a0a349bd02f320726b0b94b461f9edc1c8317
kabingusam <kabingusammy@gmail.com>
2022-12-21 12:41:08 +0300

    update on task

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,16 @@
-export default function handleResponseFromAPI(promise) {
-    promise
-    .then(() =>{
-        console.log('Got a response from the API');
-        return{
-            status: 200,
-            body: 'success',
-        };
-    }).catch(() => {
-        return new Error()
+export default function getResponseFromAPI(promise) {
+    return new Promise((resolve, reject) => {
+        promise
+        .then(() =>{
+            console.log('Got a response from the API');
+        })
+        if(promise){
+            resolve({
+                status: 200,
+                body: 'Success',
+            });
+        }else {
+            reject(new Error());
+        }
     });
 }
