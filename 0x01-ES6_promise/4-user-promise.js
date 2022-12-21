1ce18f79f101253918124c47339d818f10e3569c
kabingusam <kabingusammy@gmail.com>
2022-12-21 14:19:20 +0300

    update on the resolve issue

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,3 @@
 export function signUpUser(firstName, lastName) {
-    return Promise((resolve) => {
-        resolve({
-            firstName,
-            lastName,
-        });
-    });
+    return Promise.resolve({firstName, lastName});
 }
