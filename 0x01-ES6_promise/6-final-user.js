ed341bd1664c8b034131a1a58e359624a26c11cd
kabingusam <kabingusammy@gmail.com>
2022-12-21 15:15:05 +0300

    update on task and its completion

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,15 @@
 import { signUpUser } from './4-user-promise';
 import { uploadPhoto } from './5-photo-reject';
 
-export function handleProfileSignup(){
-    
+export function handleProfileSignup(firstName, lastName, filename){
+    return Promise
+        .allSettled(signUpUser[firstName, lastName], uploadPhoto[filename])
+            .then((results) =>{
+                return results.map((result) => {
+                    return({
+                        status: 'resolved',
+                        value: result,
+                    });
+                });
+            });
 }
