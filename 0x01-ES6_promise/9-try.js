32fd6fd3dea83a6ce75a981c80a450a30009299c
kabingusam <kabingusammy@gmail.com>
2022-12-21 16:50:23 +0300

    update

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,10 +3,10 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   }catch(error) {
-    queue.push(error.message);
+    queue.push(String(error));
   }finally{
       queue.push('Guardrail was processed');
   }
-  
+
   return queue;
 }
