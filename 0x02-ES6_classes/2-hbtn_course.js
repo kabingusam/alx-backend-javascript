2fe094af8e6bba2b9cd1de64b265852001fc0231
kabingusam <kabingusammy@gmail.com>
2022-12-23 14:08:53 +0300

    update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,41 +1,70 @@
-/*  Represents class HolbertonCourse
-*/
-class HolbertonCourse {
+/**
+ * Represents a Holberton Course.
+ */
+export default class HolbertonCourse {
+    /**
+     * Creates a new @see {@link HolbertonCourse}.
+     *
+     * @param {String} name - The name of the course.
+     * @param {Number} length - How long the course is (in months).
+     * @param {String[]} students - The names of students in the course.
+     */
     constructor(name, length, students) {
         this.name = name;
         this.length = length;
         this.students = students;
     }
 
-    get name(){
+    /**
+     * Gets the name of this course.
+     */
+    get name() {
         return this._name;
     }
-    set name(value){
-        if (typeof value !== String) {
-            throw new TypeError('name must be a string');
+
+    /**
+     * Sets the name of this course.
+     */
+    set name(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
         }
         this._name = value;
     }
 
-    get length(){
+    /**
+     * Gets the length of this course (in months).
+     */
+    get length() {
         return this._length;
     }
-    set length(value){
-        if (typeof value !== Number) {
-            throw new  TypeError('length must be a number');
+
+    /**
+     * Sets the length of this course (in months).
+     */
+    set length(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('Length must be a number');
         }
         this._length = value;
     }
 
-    get students(){
+    /**
+     * Gets the names of students in this course.
+     */
+    get students() {
         return this._students;
     }
-    set students(value){
-        if (!Array.isArray(value)) {
-            throw new TypeError('students must be an array');
+
+    /**
+     * Sets the names of students in this course.
+     */
+    set students(value) {
+        if (!(value instanceof Array)) {
+            throw new TypeError('Students must be an array of strings');
         }
-        if (!value.every(student => typeof student === 'string')) {
-            throw new TypeError('students must be an array of strings')
+        if (!value.every((student) => typeof student === 'string')) {
+            throw new TypeError('Students must be an array of strings');
         }
         this._students = value;
     }
