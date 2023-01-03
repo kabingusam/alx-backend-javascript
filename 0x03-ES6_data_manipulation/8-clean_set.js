be0df7cccdeec630dfbc72710b32e6bec5189053
kabingusam <kabingusammy@gmail.com>
2023-01-03 16:53:44 +0300

    update on task and completion

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export function cleanSet(set, startString) {
+    let result = "";
+    for (const value of set) {
+        if (value.startsWith(startString)) {
+            result += value.substring(startString.length) + "-";
+        }
+    }
+    return result.slice(0, -1);
+}
