41155eb06b4f8d75a09105a71d19bf6797d8a6f0
kabingusam <kabingusammy@gmail.com>
2023-01-03 16:12:18 +0300

    update on task and its completion

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,8 @@
 export function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
+    if (position >= length){
+        throw new Error("Position outside range")
     }
-    const buffer = new ArrayBuffer(length);
-    const view = new Int8Array(buffer);
-    view[position] = value;
+    const buffer = new Dataview(new ArrayBuffer(length), 0, length);
+    buffer.setInt8(position, value)
     return buffer;
 }
