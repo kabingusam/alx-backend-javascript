f2cc4a4ecb1e8132136bc690963c3bbeb3564c43
kabingusam <kabingusammy@gmail.com>
2023-01-06 12:58:36 +0300

    task completion

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,20 @@
+/// <reference path="./crud.d.ts" />
+import * as CRUD from "./crud";
+import {rowID, rowElement} from "./interface"
+
+const row: RowElement = {
+    firstName: "Guillaume",
+    lastName: "Salva",
+}
+
+const newRowID: RowID = CRUD.insertRow {
+    
+}
+const updatedRow: RowElement = {
+    ...row,
+    age: 23,
+}
+
+const rowId: RowID = updateRow(row.id, row);
+
+CRUD.deleteRow(rowID);
