50653a5cf755d239ce6c4827eec3b1857df87c97
kabingusam <kabingusammy@gmail.com>
2023-02-21 10:23:01 +0300

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,13 @@
-/**
- * Using Process stdin
- * 
- */
-
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
-    if (chunk !== null) {
-        const name = chunk.trim();
-        process.stdin.write(`Your name is: ${name}`);
+
+    if (chunk) {
+        process.stdout.write(`Your name is: ${chunk}`);
     }
 });
 
 process.stdin.on('end', () => {
-    process.stdin.write('\nThis important software is now closing');
+    process.stdout.write('This important software is now closing\n');
 });
