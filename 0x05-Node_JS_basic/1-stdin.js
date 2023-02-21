27dce5f8c6dfeda824cc7f85b91dbe2e6e250178
kabingusam <kabingusammy@gmail.com>
2023-02-21 10:16:42 +0300

    function inclusion

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,16 +5,16 @@
 
 process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
     if (chunk !== null) {
         const name = chunk.trim();
-        console.log(`Your name is: ${name}`);
+        process.stdin.write(`Your name is: ${name}`);
     }
 });
 
 process.stdin.on('end', () => {
-    console.log('\nThis important software is now closing');
+    process.stdin.write('\nThis important software is now closing');
 });
