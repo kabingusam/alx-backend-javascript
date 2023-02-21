4d9d617b0f6c6430f11e622f1157954f7eb81c5b
kabingusam <kabingusammy@gmail.com>
2023-02-21 10:21:03 +0300

    update on new line inclusion

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@
 
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
